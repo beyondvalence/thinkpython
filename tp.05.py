8a41171dc2c6691bffb4fa4d0472878b48044b6a
wayne <wayne.liu@nih.gov>
2016-01-29 11:21:18 -0500

    chp05, added exercise 4, triangle sides check

--- tp.05.py
+++ tp.05.py
@@ -25,7 +25,7 @@ print(text)
 
 
 #5.14 exercises
-print("5.14 E3, Fermat's Last Theorem")
+#5.14 E3, Fermat's Last Theorem
 def fermat_check(a=1,b=1,c=1,n=3):
 	a=int(input('a= '))
 	b=int(input('b= '))
@@ -55,6 +55,24 @@ def fermat_prompt():
 		else:
 			return
 			
+#5.14 E4, Triangles
+def is_triangle(a=3,b=4,c=5):
+	a=float(input("a= "))
+	b=float(input("b= "))
+	c=float(input("c= "))
+	if a < 1 or b < 1 or c < 1:
+		print("a,b,c must be positive integers")
+		is_triangle()
+	elif (a >= b + c) or (b >= a + c) or (c >= a + b):
+		print("Sides values not viable")
+		is_triangle()
+	else:
+		print("Sides make a valid triangle!")
+		return True
+	
 
 if __name__ == "__main__":			
+	print("5.14 E3, Fermat's Last Theorem")
 	fermat_prompt()
+	print("5.14 E4, Triangles")
+	is_triangle()
