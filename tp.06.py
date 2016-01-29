eb3e1ecc7e1afe95bc392660badfb857736637bb
wayne <wayne.liu@nih.gov>
2016-01-29 14:14:52 -0500

    tp06, added up to 6.2 E2,  right triange hypotenuse function for incremental development

--- tp.06.py
+++ tp.06.py
@@ -1,4 +1,37 @@
 # think python chp06, fruitful functions
 # 20160129FRI
 
-#6.1
+import math
+
+#6.1 return values
+def area(radius):
+	temp = math.pi * radius**2 
+	return temp	
+	
+#6.1 E1, compare function
+def compare(x,y):
+	if x > y:
+		return 1
+	elif x==y:
+		return 0
+	else:
+		return -1
+
+#6.2 incremental development
+#6.2 E2, right triangle hypotenuse
+def hypotenuse(left, right):
+	left_sq = left*left
+	right_sq = right*right
+	hypo_sq = left_sq + right_sq
+	return math.sqrt(hypo_sq)
+
+def hypotenuse_prompt():
+	a=float(input("leg 1 length: "))
+	b=float(input("leg 2 length: "))
+	h=hypotenuse(a, b)
+	print("the hypotenuse is: ", h)
+	return h 
+	
+hypotenuse_prompt()
+
+#6.3 composition
