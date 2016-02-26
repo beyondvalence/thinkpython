7a7f78a167d61950b45ce6a140f18ab9bf5ee63d
wayne <wayne.liu@nih.gov>
2016-02-26 12:03:04 -0500

    tp08, added up to 8.8 string methods in creating custom character parser

--- tp.08.py
+++ tp.08.py
@@ -2,20 +2,20 @@
 # 20160203WED
 
 # 8.1 fruit[n]
-print("8.1, strings as sequences")
+print("\n8.1, strings as sequences")
 fruit = 'banana'
 print(fruit)
 print(fruit[1])
 
 # 8.2 len()
-print("8.2, len function")
+print("\n8.2, len function")
 print("length of banana:", len(fruit))
 print(fruit[len(fruit)-1])
 print("or use negative indicies")
 print(fruit[-1])
 
 # 8.3 traversal with for loop
-print("8.3 loop traversal")
+print("\n8.3 loop traversal")
 index = 0
 while index < len(fruit):
 	letter = fruit[index]
@@ -23,7 +23,7 @@ while index < len(fruit):
 	index += 1
 	
 # 8.3 E1, backwards print
-print("8.3 E1, print letters backwards")
+print("\n8.3 E1, print letters backwards")
 index = 1
 while index <= len(fruit):
 	letter = fruit[-index]
@@ -31,7 +31,7 @@ while index <= len(fruit):
 	index = index + 1
 	
 # 8.3 E2, fix name spelling outputs
-print("8.3 E2, fixing name spelling outputs")
+print("\n8.3 E2, fixing name spelling outputs")
 prefixes = 'JKLMNOPQ'
 suffix = 'ack'
 for letter in prefixes:
@@ -41,17 +41,17 @@ for letter in prefixes:
 		print(letter+suffix)
 		
 # 8.4 string slices
-print("8.4 string slices")
+print("\n8.4 string slices")
 s = 'Monty Python'
 print(s[0:5])
 print(s[6:12])
 
 # 8.4 E3, slicing
-print("8.4 E3, what happens when first and second indexes are omitted")
+print("\n8.4 E3, what happens when first and second indexes are omitted")
 print(fruit[:]) # means the entire string
 
 # 8.5 immutable strings
-print("8.5 immutable strings")
+print("\n8.5 immutable strings")
 greeting = "hello, world!"
 print(greeting)
 print("greeting[0] = 'J', does not work")
@@ -60,7 +60,7 @@ new_greeting = 'J' + greeting[1:]
 print(new_greeting)
 
 # 8.6 searching
-print("8.6 searching")
+print("\n8.6 searching")
 def findd(word, letter, n=0):
 	index = n
 	print("Looking for", letter, "in", word, ":")
@@ -70,3 +70,62 @@ def findd(word, letter, n=0):
 		index += 1
 	return -1
 findd("correlation", "o")
+
+# 8.7 looping and counting
+print("\n8.7 looping and counting")
+word = 'banana'
+count = 0
+for letter in word:
+	if letter == 'a':
+		count += 1
+print("count of 'a':", count)
+
+# 8.7 E5, encapsulating count code and generalize
+print("\n8.7 E5, count as a function")
+def count(word, letter):
+	w = word.lower()
+	l = letter.lower()
+	num = 0
+	for unit in w:
+		if unit == l:
+			num += 1
+	print("Count of", letter, "in", word, ":", num)
+count("Augustus", "u")
+count("Augustus", "a")
+
+# 8.7 E6, rewrite count func to use find func
+print("\n8.7 E6, rewrite count to use find")
+def count(word, letter, start=0):
+	w = word.lower()
+	l = letter.lower()
+	loc = []
+	while start < len(w):
+		n = w.find(l,start)
+		loc.append(n)
+		start += 1
+	return set(loc)
+output = count("Augustus", "u", 0)
+print(output) # still prints out -1, when it reaches past the last letter to find, returns -1
+
+# 8.8 string methods
+print("\n8.8 string methods")
+print("\n8.8 E7, modified E6 for total counts")
+def count(word, letter, start=0):
+	w = word.lower()
+	l = letter.lower()
+	l_l = len(l)
+	loc = []
+	while start < len(w):
+		n = w.find(l,start)
+		if n == -1:
+			loc.append(-1) # non matches will output -1 so need to account for extra element in output
+			break
+		loc.append(n)
+		start = n + l_l
+	return set(loc)
+output = count("banananan", "na", 0)
+print(output)
+if -1 in output:
+	print("number found:", len(output)-1)
+else:
+	print("number found:", len(output))
